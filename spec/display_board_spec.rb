bf5544d2d135d237b805a8dff004e5ade27a3d44
Aaron Cherniavsky <aaron.b.cherniavsky@gmail.com>
2019-09-02 15:53:44 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -21,7 +21,7 @@ describe "#display_board in 'lib/display_board.rb" do
       output = capture_puts{ display_board(board) }
       rows = output.split("\n")
 
-      expect(rows[0]).to eq("   |   |   ")
+      expect(rows[0]).to eq("   |   |   ")Learn - Display Tic Tac Toe Board
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq("   | X |   ")
       expect(rows[3]).to eq("-----------")
